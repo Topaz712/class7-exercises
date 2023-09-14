c9ae1193fcafb2573fc0c0ae22bc3dd8ad1d8ed4
Topaz712 <tzam712@gmail.com>
2023-09-14 15:29:52 -0500

    added console result for exercise 3
    added the result for what was console logged for exercise 3 and added the instructions for exercise 4 to do later

--- index.js
+++ index.js
@@ -85,4 +85,12 @@ async function fetchPosts() {
 fetchPosts()
   .then((data) => {
     console.log('Fetched posts:', data);
-  });
+  });//Fetched posts: (10) [{…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}]
+
+
+//Exercise four
+//Extend the Subject class to include an async method fetchAndNotify() that retrieves the first 10 posts from the JSONPlaceholder API and then notifies its observers.
+//The data passed to each observer's update method should be the list of posts fetched.
+//Handle potential errors using a try/catch block. If an error occurs, invoke the update method of observers with an appropriate error message.
+//Within the Observer class's update method, destructure the received posts to log the title of the first post. If an error message is received instead, just log it.
+//Instantiate the Subject, add multiple observers, call fetchAndNotify(), and ensure each observer logs the title of the first post or an error message.
